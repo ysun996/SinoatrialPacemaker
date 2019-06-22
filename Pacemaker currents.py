fa4eb30edd2ca81257d8cc2d31b517081f700578
Edward Sun <ysun097@uottawa.ca>
2019-06-22 15:23:25 -0400

    Found a mistake in the code

--- Pacemaker currents.py
+++ Pacemaker currents.py
@@ -5,12 +5,14 @@ from scipy.fftpack import fft
 from scipy.signal import argrelextrema
 from common import *
 
+condition = 0
+
 state0 = [-66, 0, 0, 0, 0, 0, 0]
 
 state = odeint(PacemakerODE, state0, t, args=(parametersbase,))
 dv = []
 for points in state:
-    pacemaker = PacemakerODE(points, t, parametersbase)
+    pacemaker = PacemakerODE(points, t, parametersbase, condition)
     dv.append(pacemaker[0])
 plt.plot(t, state[:,0])
 plt.figure()

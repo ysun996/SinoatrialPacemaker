a03c1b615e4ed46325098844690b6d08f384f240
myopicmayo <51139994+myopicmayo@users.noreply.github.com>
2019-07-09 23:55:37 -0400

    Add files via upload

---
+++ updated_files/DepolarizedPhaseResponseCurve.py
@@ -0,0 +1,29 @@
+from commonJames import *
+
+
+PhaseResetYN = 1
+# Set HyperPolarYN = 1 for hyperpolarized phase response cure or 0 for depolarized phase response curve
+HyperPolarYN = 0
+if HyperPolarYN == 1:
+    PolPulse = [0.5,2.0,3.5]
+elif HyperPolarYN == 0:
+    PolPulse = [-0.6,-1.3,-1.9,-2.8]
+if PhaseResetYN == 1:
+    state = odeint(PacemakerODE, state0, t, args=(parameters,))
+    cycleLength, localMx = cycle_len(t,state)
+    print(cycleLength)
+    phasePercent = np.arange(0.05,1,0.025)
+    for w in PolPulse:
+        print('now on '+str(w))
+        phaseChange = np.zeros(len(phasePercent))
+        for i in range(len(phasePercent)):
+            phase = t[localMx[1]]+cycleLength*phasePercent[i]
+            statePulsed = pulseFnc(state0,t,parameters,phase,w)
+            cyclePulsedLength, localPulsedMx = cycle_len(t,statePulsed)
+            phaseChange[i] = (cyclePulsedLength-cycleLength)/cycleLength*100
+            
+        plt.plot(phasePercent*100,phaseChange, 'o',label=str(w) + '\u03BC'+'A/c$m^2$')
+    plt.legend()
+    plt.ylabel('\u0394'+'$\phi$'+' (%)')
+    plt.xlabel('% of Cycle ($\phi$)')
+    plt.show()
